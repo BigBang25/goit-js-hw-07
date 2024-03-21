947be4f3e5e031ceaf32d3134a517842fe5034c6
BigBang25 <isa5528@yahoo.com>
2024-03-21 17:52:27 +0100

    hw7fin

---
+++ js/task-5.js
@@ -0,0 +1,18 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+console.log(getRandomHexColor());
+
+const colorBtn = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const colorSpan = document.querySelector(".color");
+
+colorBtn.addEventListener("click", setRandomColor);
+
+function setRandomColor(event) {
+  body.style.backgroundColor = getRandomHexColor();
+  colorSpan.textContent = body.style.backgroundColor;
+}
